323e829181370c93979852619cb7bb9ecc0910c9
Berkay052 <167540574+Berkay3452@users.noreply.github.com>
2024-10-12 14:36:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿
+Console.WriteLine("11.10.2024 - 21.10.2024 Tarihli Ödev");
+Console.WriteLine("-------------------------------------");
+
+
+Console.WriteLine("\n");
+
+/*
+
+Console.WriteLine("İki sayının farkını bulan C# kodu");
+Console.WriteLine("----------------------------------");
+
+
+int sayi_1, sayi_2, fark;
+
+Console.WriteLine("Lütfen 1. sayıyı giriniz: ");                          
+sayi_1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Lütfen 2. sayıyı giriniz: ");
+sayi_2 = Convert.ToInt32(Console.ReadLine());
+
+fark = sayi_1 - sayi_2;
+
+Console.WriteLine("Bu İki Sayının Farkının Sonucu: " + fark);
+
+*/
+
+
+
+
+/*
+
+Console.WriteLine("5 sayısının karesini bulan C# kodu");
+Console.WriteLine("-----------------------------------");
+
+
+int a = 5;
+
+double sonuc = Math.Pow(a, 2);
+
+Console.WriteLine("5'in karesi: " + sonuc);
+
+*/
+
+
+
+
+/*
+
+Console.WriteLine("10 sayısının 3'e bölümünden kalanını bulan C# kodu");
+Console.WriteLine("---------------------------------------------------");
+
+
+int a = 10, b = 3, kalan;
+
+kalan = a % b;
+
+Console.WriteLine("10 sayısının 3'e bölümünden kalan: " + kalan);
+
+*/
+
+
+
+
+/*
+
+Console.WriteLine("Klavyeden girilen 4 sayının toplamını ve çarpımını bulan C# kodu");
+Console.WriteLine("-----------------------------------------------------------------");
+
+
+int sayi_1, sayi_2, sayi_3, sayi_4, toplam, carpım;
+
+Console.WriteLine("Lütfen 1. sayıyı giriniz: ");
+sayi_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen 2. sayıyı giriniz: ");
+sayi_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen 3. sayıyı giriniz: ");
+sayi_3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen 4. sayıyı giriniz: ");
+sayi_4 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\n");
+
+toplam = sayi_1 + sayi_2 + sayi_3 + sayi_4;
+
+Console.WriteLine("Girilen 4 Sayının Toplam Sonucu: " + toplam);
+
+Console.WriteLine("\n");
+
+carpım = sayi_1 * sayi_2 * sayi_3 * sayi_4;
+
+Console.WriteLine("Girilen 4 Sayının Çarpım Sonucu: " + carpım);
+
+*/
+
+
+
+/*
+
+Console.WriteLine("Klavyeden girilen 2 sayının bölümünü bulan C# kodu");
+Console.WriteLine("---------------------------------------------------");
+
+Console.WriteLine("\n");
+
+int sayi_1, sayi_2, bölüm;
+
+Console.WriteLine("Lütfen 1. sayıyı giriniz: ");
+sayi_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen 2. sayıyı giriniz: ");
+sayi_2 = Convert.ToInt32(Console.ReadLine());
+
+bölüm = sayi_1 / sayi_2;
+
+Console.WriteLine("Girilen İki Sayının Bölümünün Sonucu: " + bölüm);
+
+*/
+
+
+
+
+/*
+
+Console.WriteLine("Klavyeden kenarları girilen dikdörtgenler prizmasının hacmini hesaplayan C# kodu");
+Console.WriteLine("---------------------------------------------------------------------------------");
+
+int uzunluk, genislik, yukseklik, hacim;
+
+Console.WriteLine("Lütfen Dikdörtgenin Uzunluğunu Giriniz: ");
+uzunluk = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen Dikdörtgenin Genişliğini Giriniz: ");
+genislik = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Lütfen Dikdörtgenin Yüksekliğini Giriniz: ");
+yukseklik = Convert.ToInt32(Console.ReadLine());
+
+hacim = uzunluk * genislik * yukseklik;
+
+Console.WriteLine("Dikdörtgenin Hacmi: " + hacim);
+
+*/
+
+Console.ReadKey();
+
+
